eb4901856a4b9d9c09f52fcda400b3f3f95995c4
VanDong <nguyenvandong1108@gmail.com>
2018-11-04 14:45:04 +0700

    map:curent location.

--- app/src/main/java/com/vandong/demogooglemap/MainActivity.java
+++ app/src/main/java/com/vandong/demogooglemap/MainActivity.java
@@ -1,6 +1,13 @@
 package com.vandong.demogooglemap;
 
+import android.Manifest;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,13 +20,15 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements
+        OnMapReadyCallback {
+
     private GoogleMap mMap;
     Spinner spinnerType;
     ArrayList<String> dsType;
@@ -34,11 +43,17 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         addEvents();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
     public void addControls() {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        spinnerType = (Spinner) findViewById(R.id.spinner);
+        spinnerType = findViewById(R.id.spinner);
         dsType = new ArrayList<>();
         dsType.addAll(Arrays.asList(getResources().getStringArray(R.array.arrType)));
         adapterType = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item, dsType);
@@ -50,7 +65,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         progressDialog.setMessage("Đang tải dữ liệu , vui lòng đợi...");
         progressDialog.setCanceledOnTouchOutside(false);
         progressDialog.show();
-
     }
 
     public void addEvents() {
@@ -65,7 +79,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
             }
         });
-
     }
 
     private void xuLyDoiCheDoHienThi(int position) {
@@ -76,18 +89,27 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             case 1:
                 mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
                 break;
-            case 3:
+            case 2:
                 mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
                 break;
-            case 4:
+            case 3:
                 mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-                break;
-            case 5:
-                mMap.setMapType(GoogleMap.MAP_TYPE_NONE);
                 break;
         }
 
     }
+
+    GoogleMap.OnMyLocationChangeListener listener = new GoogleMap.OnMyLocationChangeListener() {
+        @Override
+        public void onMyLocationChange(Location location) {
+            LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
+            if (mMap != null) {
+                mMap.clear();
+                Marker marker = mMap.addMarker(new MarkerOptions().position(loc));
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 16));
+            }
+        }
+    };
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -98,9 +120,17 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 progressDialog.dismiss();
             }
         });
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(11.888259, 108.335090);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,17));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnMyLocationChangeListener(listener);
     }
 }
